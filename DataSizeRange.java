4196e2574b00714d635bfaf251194967818dc455
Chirag singh <chiragsingh8926@gmail.com>
2024-04-22 17:44:07 +0530

    Finding The Range Of Different Data Types

---
+++ DataSizeRange.java
@@ -0,0 +1,39 @@
+//To Find The Range of Different Data Types
+//
+import java.util.Scanner;
+class DataSizeRange {
+    public static void main(String args[]) {
+        // Short
+        System.out.println("Short Min: " + Short.MIN_VALUE);
+        System.out.println("Short Max: " + Short.MAX_VALUE);
+        System.out.println("Short Bytes: " + Short.BYTES);
+        
+        // Integer
+        System.out.println("Integer Min: " + Integer.MIN_VALUE);
+        System.out.println("Integer Max: " + Integer.MAX_VALUE);
+        System.out.println("Integer Bytes: " + Integer.BYTES);
+        
+        // Long
+        System.out.println("Long Min: " + Long.MIN_VALUE);
+        System.out.println("Long Max: " + Long.MAX_VALUE);
+        System.out.println("Long Bytes: " + Long.BYTES);
+        
+        // Float
+        System.out.println("Float Min: " + Float.MIN_VALUE);
+        System.out.println("Float Max: " + Float.MAX_VALUE);
+        System.out.println("Float Bytes: " + Float.BYTES);
+        
+        // Double
+        System.out.println("Double Min: " + Double.MIN_VALUE);
+        System.out.println("Double Max: " + Double.MAX_VALUE);
+        System.out.println("Double Bytes: " + Double.BYTES);
+        
+        // Character
+        System.out.println("Character Min: " + Character.MIN_VALUE);
+        System.out.println("Character Max: " + Character.MAX_VALUE);
+        System.out.println("Character Bytes: " + Character.BYTES);
+        
+        // Boolean
+        System.out.println("Boolean: " + Boolean.TRUE + ", " + Boolean.FALSE);
+    }
+}
