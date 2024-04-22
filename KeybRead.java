8b5a326eaf46d82375afe7add340f91a4275bca9
Chirag singh <chiragsingh8926@gmail.com>
2024-04-22 17:21:34 +0530

    Reading From KeyBoard Program

--- KeybRead.java
+++ KeybRead.java
@@ -2,15 +2,16 @@
 //
 import java.lang.*;
 import java.util.*;
+import java.util.Scanner;
 
 class KeybRead
 {
-    public static void main (String args{})
+    public static void main(String[] args)
     {
         Scanner S= new Scanner (System.in);
         String name;
         System.out.println ("May I know Your Name");
         name = S.nextLine();
-        System.out.println ("Welcome Mr/Mrs"+ name);
+        System.out.println ("Welcome Mr/Mrs " + name);
         } 
 }
