98b2d3604f79a532dda6c95c931f28dc736dfb1a
Chirag singh <chiragsingh8926@gmail.com>
2024-04-22 17:07:00 +0530

    Reading From KeyBoard Program

---
+++ KeybRead.java
@@ -0,0 +1,16 @@
+//Reading From Keyboard
+//
+import java.lang.*;
+import java.util.*;
+
+class KeybRead
+{
+    public static void main (String args{})
+    {
+        Scanner S= new Scanner (System.in);
+        String name;
+        System.out.println ("May I know Your Name");
+        name = S.nextLine();
+        System.out.println ("Welcome Mr/Mrs"+ name);
+        } 
+}
