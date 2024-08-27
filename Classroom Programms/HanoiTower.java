cb7ed038fe4eca8c92596e48ba94f4d76a6594a7
Chirag singh <chiragsingh8926@gmail.com>
2024-08-28 01:30:00 +0530

    Update Hanoi Tower File

--- Classroom Programms/HanoiTower.java
+++ Classroom Programms/HanoiTower.java
@@ -4,11 +4,13 @@
 import java.io.*;
 import java.math.*;
 import java.util.*;
-class hanoiTower {
+class hanoiTower 
+{
 	static void towerOfHanoi(int n, char from_rod,char to_rod, char aux_rod)
 							
 	{
-		if (n == 0) {
+		if (n == 0) 
+		{
 			return;
 		}
 		towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
