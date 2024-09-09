b3743d15d0b9aaa88b7392329097a17a2633cb9f
Chirag singh <chiragsingh8926@gmail.com>
2024-09-09 22:10:43 +0530

    Interview Sheet 1:- Q1

---
+++ #Interview Sheet1/ReverseString.java
@@ -0,0 +1,26 @@
+// Q1.How do you reverse a string in Java?
+// Interview Sheet 1 Question's
+// 
+
+public class ReverseString 
+{
+    public static void main(String[] args) {
+		String str = "123";
+
+		System.out.println(reverse(str));
+	}
+
+	public static String reverse(String in) {
+		if (in == null)
+			throw new IllegalArgumentException("Null is not valid input");
+
+		StringBuilder out = new StringBuilder();
+
+		char[] chars = in.toCharArray();
+
+		for (int i = chars.length - 1; i >= 0; i--)
+			out.append(chars[i]);
+
+		return out.toString();
+	}
+}
