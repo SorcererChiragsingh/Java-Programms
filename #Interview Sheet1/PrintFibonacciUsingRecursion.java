51ae593cd4f9cf5b9a7e8799a4810be742695720
Chirag singh <chiragsingh8926@gmail.com>
2024-10-28 22:39:25 +0530

    Interview Sheet 1:- Q5 done

---
+++ #Interview Sheet1/PrintFibonacciUsingRecursion.java
@@ -0,0 +1,65 @@
+// Q5. Write a Java program to print a Fibonacci sequence using recursion.
+// Interview Sheet 1 Question's
+// 
+
+// Easy 
+// T.C =
+// S.C =
+
+/* 
+Explanation:-
+
+A Fibonacci sequence is one in which each number is the sum of the two previous numbers. 
+In this example, the sequence begins with 0 and 1. The following example code shows how to use a for loop to print a Fibonacci sequence:
+*/
+
+public class PrintFibonacciUsingRecursion 
+{
+
+	public static void printFibonacciSequence(int count) 
+    {
+		int a = 0;
+		int b = 1;
+		int c = 1;
+
+		for (int i = 1; i <= count; i++) 
+        {
+			System.out.print(a + ", ");
+
+            a = b;
+			b = c;
+			c = a + b;
+		}
+	}
+
+	public static void main(String[] args) 
+    {
+    	printFibonacciSequence(10);
+	}
+
+}
+
+/**
+The following example class shows how to use recursion to calculate a Fibonacci sequence that is 10 numbers long:
+
+public class PrintFibonacciUsingRecursion  {
+
+    public static int fibonacci(int count) {
+		if (count <= 1)
+			return count;
+
+		return fibonacci(count - 1) + fibonacci(count - 2);
+	}
+
+	public static void main(String args[]) {
+    	int seqLength = 10;
+
+    	System.out.print("A Fibonacci sequence of " + seqLength + " numbers: ");
+
+    	for (int i = 0; i < seqLength; i++) {
+      	    System.out.print(fibonacci(i) + " ");
+    	}
+  	}
+
+}
+*/
